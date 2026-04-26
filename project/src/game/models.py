9af3aeb49d9e7926e3ca82bbc92182662482d958
lats1e <wolflatsie@gmail.com>
2026-04-26 19:34:55 -0400

    Revert "added attention"
    This reverts commit e5e15ca47235ee433dcd0e57335d3c7efaf90d09.

--- project/src/game/models.py
+++ project/src/game/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Player(models.Model):
     name = models.CharField(max_length=100)
     score = models.IntegerField(default=0)
-    attention = models.FloatField(default=0.0)
 
     def __str__(self):
         return self.name
